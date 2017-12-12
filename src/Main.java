addc0f7d5ce90a85b8fedf84a19a06e66c1d1170
USER <105021060@live.asia.edu.tw>
2017-12-12 12:06:32 +0800

    887

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
-    public static void main(String[] args ){
-        MainFrame mFrm=new MainFrame();
-        mFrm.setVisible(true);
+
+    public static void main(String[] args) {
+	    MainFrame mainFrame=new MainFrame();
+	    mainFrame.setVisible(true);
     }
 }
